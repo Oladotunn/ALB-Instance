f6d75b8511f87a6707c4bbb758122a40cb70d7f3
Oladotunn <51125870+Oladotunn@users.noreply.github.com>
2019-07-31 09:45:53 +0100

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -44,7 +44,6 @@ resource "aws_lb_listener" "aqua_listener" {
 }
 
 resource "aws_autoscaling_attachment" "alb_autoscale" {
-  alb_target_group_arn   = aws_lb_target_group.aqua_target_group.arn
   autoscaling_group_name = aws_autoscaling_group.autoscaling.id
 }
 
